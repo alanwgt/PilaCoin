8ded3a26cabec2530472e22aa7b27188d0744166
Alan Weingartner <alan.weingartner@gmail.com>
2018-05-23 11:21:27 -0300

    added model

---
+++ src/main/java/com/alanwgt/helpers/PilaTransfModel.java
@@ -0,0 +1,20 @@
+package com.alanwgt.helpers;
+
+public class PilaTransfModel {
+
+    private String to, value;
+
+    public PilaTransfModel(String to, String value) {
+        this.to = to;
+        this.value = value;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
